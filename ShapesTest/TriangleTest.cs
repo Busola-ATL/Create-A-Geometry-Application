502708eafa6dc6b1a223b5b892a0a204064df348
Busola-ATL <156509764+Busola-ATL@users.noreply.github.com>
2024-04-17 22:12:45 +0000

    Triangle test added

--- ShapesTest/TriangleTest.cs
+++ ShapesTest/TriangleTest.cs
@@ -1,3 +1,26 @@
+
+using GeometryApp.ShapesLib;
+using ShapesLib;
 namespace GeometryApp.ShapesTest;
 
-using ShapesLib;
+
+
+[TestClass]
+public class TriangleTest
+{
+    [TestMethod]
+    public void TestArea()
+    {
+        Triangle triangle = new Triangle(8, 3);
+
+        //calling area method
+        var result = triangle.CalculateArear();
+
+        //calling square method
+       var result1 = triangle.CalculatePerimeter();
+
+        //Assert
+        Assert.AreEqual(12, result);
+        Assert.AreEqual(18, result1);
+    }
+}
