35e28ac1f6ae5fb9622b8eba88e532561108e5d9
Busola-ATL <156509764+Busola-ATL@users.noreply.github.com>
2024-04-17 21:56:03 +0000

    Square added

--- ShapesLib/Square.cs
+++ ShapesLib/Square.cs
@@ -1,6 +1,26 @@
-﻿// namespace GeometryApp.ShapesLib;
+﻿namespace GeometryApp.ShapesLib;
+using GeometryApp.ShapesLib.Interfaces;
 
-// public class Square : 
-// {
+public class Square : IShape
+{
+    private double lenght;
 
-// }
+    public Square(double lenght){
+        this.lenght = lenght;
+        
+    }
+
+    public double CalculateArea()
+    {
+        throw new NotImplementedException();
+    }
+
+    public double CalculateArear(){
+        return Math.Pow(lenght, 2);
+    }
+
+    public double CalculatePerimeter(){
+        return 2 * (lenght + lenght);
+    }
+
+}
