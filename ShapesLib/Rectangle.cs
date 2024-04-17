fd556947a4294c4e23d6b27d238f0cb9952547e4
Busola-ATL <156509764+Busola-ATL@users.noreply.github.com>
2024-04-17 21:54:39 +0000

    Rectagle class implemented IShape

--- ShapesLib/Rectangle.cs
+++ ShapesLib/Rectangle.cs
@@ -1,4 +1,26 @@
-﻿namespace Create.GeometryApp.ShapesLib;
-
+﻿namespace GeometryApp.ShapesLib;
+using Interfaces;
 
 public class Rectangle : IShape
+{
+    private double lenght;
+    private double breadth;
+
+    public Rectangle(double lenght, double breadth){
+        this.lenght = lenght;
+        this.breadth = breadth; 
+    }
+
+    public double CalculateArea()
+    {
+        throw new NotImplementedException();
+    }
+
+    public double CalculateArear(){
+        return lenght * breadth;
+    }
+
+    public double CalculatePerimeter(){
+        return 2 * (lenght + breadth);
+    }
+}
